7fe099cc1aad317edbc60c6a643973726e3e13e2
Satoshi Iwai <siwai@lenovo.com>
2024-01-24 19:03:15 +0900

    End of chapter 16

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,14 @@
 import CardWrapper, { Card } from "../ui/dashboard/cards"
 import RevenueChart from "../ui/dashboard/revenue-chart"
 import LatestInvoices from "../ui/dashboard/latest-invoices"
+import { Metadata } from "next"
 import { lusitana } from "../ui/fonts"
 import { Suspense } from "react"
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from "@/app/ui/skeletons"
 
+export const metadata: Metadata = {
+    title: 'Dashboard',
+}
 export default async function Page() {
     return (
         <main>
